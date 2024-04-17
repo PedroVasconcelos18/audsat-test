e8556ed22c397e4e6cd30db15d45460dd72a8fd4
Pedro Vasconcelos <pedrovasconcelos.dev@outlook.com>
2024-04-16 22:21:19 -0300

    commit inicial

---
+++ src/main/java/io/pedrovasconcelosdev/audsattest/AudsattestApplication.java
@@ -0,0 +1,13 @@
+package io.pedrovasconcelosdev.audsattest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudsattestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudsattestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pedrovasconcelosdev/audsattest/AudsattestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pedrovasconcelosdev.audsattest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudsattestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
